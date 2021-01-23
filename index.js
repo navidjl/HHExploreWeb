67ed655022634d31f4d9b383fae8f1b3adcfef62
Navid Jalili <navidjalili@gmail.com>
2021-01-22 23:14:24 -0500

    port change

--- index.js
+++ index.js
@@ -180,7 +180,7 @@ app.post('/uploadimage', (req, res) => {
    });
 })
 
-var server = app.listen(8000, function () {
+var server = app.listen(process.env.PORT || 8000, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
